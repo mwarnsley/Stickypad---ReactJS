b7d14876a0ae9f9194bd758f2d308622c5be28ee
Warnsley <Marcus.Warnsley@navient.com>
2016-04-17 23:54:34 -0400

    Editing the API file in order to have the note saved to the mongolab databse

--- src/js/utils/appAPI.js
+++ src/js/utils/appAPI.js
@@ -1,5 +1,12 @@
 var AppActions = require('../actions/AppActions');
 
 module.exports = {
-	
+	addNote: function(note){
+		$.ajax({
+			url: "https://api.mongolab.com/api/1/databases/mystickypad/collections/notes?apikey={{THIS IS MY API KEY}}",
+			data: JSON.stringify(note),
+			type: "POST",
+			contentType: "application/json"
+		});
+	}
 }
