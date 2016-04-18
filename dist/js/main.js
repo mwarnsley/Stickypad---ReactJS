889302f9c181f90b9ea7c3f8300a10c49cadec7a
Warnsley <Marcus.Warnsley@navient.com>
2016-04-17 23:40:23 -0400

    Adding the Add Note Form JS and editing the app.js file

--- dist/js/main.js
+++ dist/js/main.js
@@ -19819,42 +19819,6 @@ var CHANGE_EVENT = 'change';
 
 var _notes = [];
 
-var AppStore = assign({}, EventEmitter.prototype, {
-	addNote: function(note){
-		_notes.push(note);
-	},
-	getNotes: function(){
-		return _notes;
-	},
-	emitChange: function(){
-		this.emit(CHANGE_EVENT);
-	},
-	addChangeListener: function(callback){
-		this.on('change', callback);
-	},
-	removeChangeListener: function(callback){
-		this.removeListener('change', callback);
-	}
-});
-
-AppDispatcher.register(function(payload){
-	var action = payload.action;
-
-	switch(action.actionType){
-		case AppConstants.ADD_NOTE:
-		console.log('Adding Note...');
-
-		// Store Save
-		AppStore.addNote(action.note);
-
-		// API Save
-
-		// Emit Change
-		AppStore.emit(CHANGE_EVENT);
-	}
-
-	return true;
-});
 
 module.exports = AppStore;
 
