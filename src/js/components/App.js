8fc3168e7317951c5315c7e074160a8918278a05
Warnsley <Marcus.Warnsley@navient.com>
2016-04-17 23:33:15 -0400

    First commit adding the initial file structure and setting up the boilerplate and template for the project

---
+++ src/js/components/App.js
@@ -0,0 +1,6 @@
+var React = require('react');
+var AppActions = require('../actions/AppActions');
+var AppStore = require('../stores/AppStore');
+var AddNoteForm = require('./AddNoteForm.js');
+
+module.exports = App;
---
+++ src/js/stores/AppStore.js
@@ -0,0 +1,12 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var AppConstants = require('../constants/AppConstants');
+var EventEmitter = require('events').EventEmitter;
+var assign = require('object-assign');
+var AppAPI = require('../utils/AppAPI.js');
+
+var CHANGE_EVENT = 'change';
+
+var _notes = [];
+
+
+module.exports = AppStore;
