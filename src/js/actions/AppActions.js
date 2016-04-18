723b5ad3bebaeeaca7c894f250fa1df85ab69fbd
Warnsley <Marcus.Warnsley@navient.com>
2016-04-17 23:45:19 -0400

    Adding the stores js file and setting up the actions for adding the notes

--- src/js/actions/AppActions.js
+++ src/js/actions/AppActions.js
@@ -2,7 +2,10 @@ var AppDispatcher = require('../dispatcher/AppDispatcher');
 var AppConstants = require('../constants/AppConstants');
 
 var AppActions = {
-
+	AppDispatcher.handleViewAction({
+		actionType: AppConstants.ADD_NOTE,
+		note: note
+	});
 }
 
 module.exports = AppActions;
