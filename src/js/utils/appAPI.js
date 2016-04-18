eb06f395dd1830b32be1cbe3dc16bfa3040ab7d8
Warnsley <Marcus.Warnsley@navient.com>
2016-04-17 23:57:18 -0400

    Adding the ajax call to get the data and receive the notes

--- src/js/utils/appAPI.js
+++ src/js/utils/appAPI.js
@@ -8,5 +8,18 @@ module.exports = {
 			type: "POST",
 			contentType: "application/json"
 		});
+	},
+	getNotes: function(){
+		$.ajax({
+			url: "https://api.mongolab.com/api/1/databases/mystickypad/collections/notes?apikey={{THIS IS MY API KEY}}",
+			dataType: "json",
+			cache: false,
+			success: function(data){
+				AppActions.receiveNotes(data);
+			}.bind(this),
+			error: function(xhr, status, err){
+				console.log(err);
+			}.bind(this)
+		});
 	}
 }
